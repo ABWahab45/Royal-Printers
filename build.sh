ae5d754413c6b8abbb55b9062aa3b9683ad68568
ABWahab45 <aw226861@gmail.com>
2025-07-28 16:23:27 +0500

    update

--- build.sh
+++ build.sh
@@ -1,16 +1,10 @@
 #!/bin/bash
 
-# Set execute permissions for react-scripts
-chmod +x node_modules/.bin/react-scripts
-
 # Navigate to frontend directory
 cd frontend
 
 # Install dependencies
 npm install
 
-# Set execute permissions for react-scripts in frontend
-chmod +x node_modules/.bin/react-scripts
-
-# Build the project
-npm run build
+# Use npx to run react-scripts build (bypasses permission issues)
+npx react-scripts build
